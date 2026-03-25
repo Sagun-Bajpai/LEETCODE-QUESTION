b6c49e17c0bb858db8a68e0b8c44a81c2ba4e5c4
Sagun Bajpai <sagunbajpaisagun@gmail.com>
2026-03-25 23:26:12 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (49.35%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,8 +1,10 @@
 class Solution {
     public int findPeakElement(int[] nums) {
+        //using binary search
         int n=nums.length;
         int st=0;
         int end=n-1;
+        int index=-1;
         while(st<=end){
             int mid=(st+(end-st)/2);
             if((mid==0||nums[mid]>nums[mid-1])  &&  (mid==n-1||nums[mid]>nums[mid+1])){
@@ -16,7 +18,7 @@ class Solution {
             }
 
         }
-        return -1;
+        return index;
         
     }
 }
