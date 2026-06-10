fb305b0f000bfe46f52e33a4fa54560d6b196fc6
Sagun Bajpai <sagunbajpaisagun@gmail.com>
2026-06-10 11:54:24 +0530

    Time: 0 ms (100.00%) | Memory: 45.3 MB (22.44%) - LeetSync

--- 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.java
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.java
@@ -3,14 +3,12 @@
  * public class ListNode {
  *     int val;
  *     ListNode next;
- *     ListNode(int x) { val = x; }
+ *     ListNode(int x) { val = x; }// esme listNode user define data type hai
  * }
  */
 class Solution {
     public void deleteNode(ListNode node) {
         node.val=node.next.val;//overwrite next ko curr me
-        node.next=node.next.next;//skip karenge
-       
-        
+        node.next=node.next.next;//connect krr rhe a.next->a.next.next
     }
 }
