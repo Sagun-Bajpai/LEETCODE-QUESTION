0fcf159931d40984aec17746155c4f3a4eb16553
Sagun Bajpai <sagunbajpaisagun@gmail.com>
2026-06-20 10:54:17 +0530

    Time: 5 ms (44.00%) | Memory: 94.3 MB (66.26%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -10,36 +10,43 @@
  */
 class Solution {
     //reverse method
-    public ListNode reverse(ListNode head){
+    public static ListNode reverse(ListNode head){
+        // ListNode temp=head;
+        // if(temp==null) return null;
+        // reverse(temp.next);
+        // System.out.print(temp.val+" ");
         ListNode prev=null;
         ListNode after=null;
         ListNode curr=head;
         while(curr!=null){
-            after=curr.next;
+            after= curr.next;
             curr.next=prev;
             prev=curr;
             curr=after;
 
         }
         return prev;
+
     }
-    public boolean isPalindrome(ListNode head) {
-       ListNode slow=head;
-       ListNode fast=head;
-       while(fast.next!=null && fast.next.next!=null){
-        slow=slow.next;
-        fast=fast.next.next;
-       }
-       //slow is a left mid of linkedlist and we are reverse slow.next total remaining list
-       ListNode temp=reverse(slow.next);
-       slow.next=temp;
-       ListNode p1=head;
-       ListNode p2=slow.next;
-       while(p2!=null){
-        if(p1.val!=p2.val) return false;
-        p1=p1.next;
-        p2=p2.next;
-       }
-       return true;
+    public boolean isPalindrome(ListNode head){
+        ListNode slow=head;
+        ListNode fast=head;
+        if(head==null || head.next==null) return true;
+        while(fast.next!=null && fast.next.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        ListNode temp=reverse(slow.next);
+        ListNode p1=head;
+        ListNode p2=temp;
+        while(p2!=null){
+            if(p1.val != p2.val){
+            return false;
+            }
+            p1=p1.next;
+            p2=p2.next;;
+        }
+        
+        return true;
     }
 }
