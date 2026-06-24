dd06a655a337aca1702acdfdbf00f2ed6a92dbe0
Sagun Bajpai <sagunbajpaisagun@gmail.com>
2026-06-24 13:45:01 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (31.07%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -14,52 +14,60 @@ class Node {
 */
 
 class Solution {
-    public Node copyRandomList(Node head) {
+    public Node copyRandomList(Node head1) {
+        Node temp1=head1;
         Node head2=new Node(0);
         Node temp2=head2;
-        Node temp1=head;
-        //creating deep copy
+        //deep copy of node
         while(temp1!=null){
             Node t=new Node(temp1.val);
             temp2.next=t;
             temp2=t;
             temp1=temp1.next;
-        }  
-        head2=head2.next;
-        temp2=head2; 
-        temp1=head;  
-        //alterbate connections
+        }
+       head2=head2.next;
+       temp2=head2;
+       temp1=head1;
+
+        //alternate connection
         Node temp=new Node(-1);
         while(temp1!=null){
             temp.next=temp1;
             temp1=temp1.next;
             temp=temp.next;
-            
+                
             temp.next=temp2;
             temp2=temp2.next;
             temp=temp.next;
         }
+        //assigning random node
+         temp2=head2;
+       temp1=head1;
 
-        temp2=head2; 
-        temp1=head; 
-        //assigning random pointer
         while(temp1!=null){
-            if(temp1.random==null) temp2.random=null;
-            else temp2.random=temp1.random.next;
-            temp1=temp1.next.next;//// original list me 2 steps aage
-            if(temp1!=null) temp2=temp2.next.next;//// copy list me bhi 2 steps aage
+            if(temp1.random==null){
+                temp2.random=null;
+            }
+            else{
+                temp2.random=temp1.random.next;
+
+            }
+            temp1=temp1.next.next;
+            if(temp1!=null){  
+                temp2=temp2.next.next;
+            }
         }
 
-        temp2=head2; 
-        temp1=head; 
-        //seperating the list
+        //split the node
+         temp2=head2;
+       temp1=head1;
         while(temp1!=null){
             temp1.next=temp2.next;
             temp1=temp1.next;
-            if(temp1==null) break;
-            temp2.next=temp1.next;
             if(temp2.next==null) break;
+            temp2.next=temp1.next;
             temp2=temp2.next;
+        
         }
         return head2;
 
