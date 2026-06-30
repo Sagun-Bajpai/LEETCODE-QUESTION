8037490aa732ced920776fcf63f55863bb9cff4d
Sagun Bajpai <sagunbajpaisagun@gmail.com>
2026-06-30 23:25:49 +0530

    Time: 3 ms (60.71%) | Memory: 44.1 MB (84.39%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -3,14 +3,17 @@ class Solution {
         int st=0;
         int end=s.length()-1;
         while(st<end){
-            while(st<end && !Character.isLetterOrDigit(s.charAt(st))){//Character.isLetterOrDigit(c)   built in method jo check krta hai letter ya digit hai ki nhi
+            while(st<end && !Character.isLetterOrDigit(s.charAt(st))){
                 st++;
             }
+
             while(st<end && !Character.isLetterOrDigit(s.charAt(end))){
                 end--;
             }
-            if(Character.toLowerCase(s.charAt(st))!=Character.toLowerCase(s.charAt(end))){//small letter(start) !=small letter(end) 
-              return false; 
+
+            if(Character.toLowerCase(s.charAt(st)) !=Character.toLowerCase(s.charAt(end))){
+                return false;
+                
             }
             st++;
             end--;
