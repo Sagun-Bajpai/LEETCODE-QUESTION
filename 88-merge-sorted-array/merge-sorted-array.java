da6a3c18ff06cbbc22bb72d84f17832842017d9e
Sagun Bajpai <sagunbajpaisagun@gmail.com>
2026-07-03 19:48:44 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (61.32%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -14,30 +14,33 @@ class Solution {
         int i=m-1;
         int j=n-1;
         int k=m+n-1;
-       
+
         while(i>=0 && j>=0){
             if(nums1[i]>=nums2[j]){
                 nums1[k]=nums1[i];
-                k--;
                 i--;
+                k--;
             }
             else{
                 nums1[k]=nums2[j];
-                k--;
                 j--;
+                k--;
             }
+           
+
         }
         while(i>=0){
-            nums1[k]=nums1[i];
-            k--;
+             nums1[k]=nums1[i];
             i--;
-        }
-        while(j>=0){
-            nums1[k]=nums2[j];
             k--;
-            j--;
+
         }
 
+        while(j>=0){
+            nums1[k]=nums2[j];
+            j--;
+            k--;
+        }
         
     }
 }
