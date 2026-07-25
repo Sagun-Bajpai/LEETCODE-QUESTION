9d47c7509ca3722f7d84fb762469a69c43a0fa2c
Sagun Bajpai <sagunbajpaisagun@gmail.com>
2026-07-25 10:21:02 +0530

    Time: 2 ms (49.80%) | Memory: 52.7 MB (63.64%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -10,41 +10,41 @@
  * }
  */
 public class Solution {
-    //helper function  fine size
-    public int getSize(ListNode head){
-            int count=0;
-            ListNode temp=head;
-            while(temp!=null){
-                count++;
-                temp=temp.next;
-
-            }
-            return count;
+    public static int getSize(ListNode head){
+        int count=0;
+        ListNode curr=head;
+        while(curr!=null){
+            count++;
+           curr=curr.next;
+            
         }
-        
+        return count;
+    }
+ 
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        int sizeA=getSize(headA);
-        int sizeB=getSize(headB);
-        int diff=sizeA-sizeB;
-        ListNode listA=headA;
-        ListNode listB=headB;
+        int size1=getSize(headA);
+       int  size2=getSize(headB);
+        int diff=size1-size2;
         if(diff>0){
-            for(int i=1;i<=diff;i++){
-            listA=listA.next;
+            for(int i=0;i<diff;i++){
+                headA=headA.next;
             }
-
         }
         else{
-            for(int i=1;i<=Math.abs(diff);i++){
-            listB=listB.next;
+            for(int i=0;i<Math.abs(diff);i++){
+                headB=headB.next;
+            }
+        }
+        while(headA!=null && headB!=null){
 
+            if(headA==headB){
+                return headA;
+            }
+            else{
+                headA=headA.next;
+                headB=headB.next;
+            }
         }
-        }
-        while(listA!=listB){
-            listA=listA.next;
-            listB=listB.next;
-
-        }
-       return listA; 
+        return headA;
     }
 }
