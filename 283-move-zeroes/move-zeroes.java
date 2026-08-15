9667d40560f9d4cfb4eb89d424c86c5755a9d557
Sagun Bajpai <sagunbajpaisagun@gmail.com>
2026-08-16 01:44:18 +0530

    Time: 159 ms (5.36%) | Memory: 46.9 MB (99.69%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,18 +1,20 @@
 class Solution {
-    public void moveZeroes(int[] nums) {
-        int left=0;
-        //int right=nums.length-1;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]!=0){
-                // i ko left se compare karenge jisme non zero hoga usko 0 se replace krr dega;
-                int temp=nums[left];
-                nums[left]=nums[i];
-                nums[i]=temp;
-                left++;
-               
-                
+    public void moveZeroes(int[] arr) {
+        int n=arr.length;
+        int i=0;
+        int j=0;
+        while(j<=n-1){
+             System.out.println("j = " + j + ", i = " + i);
+            if(arr[j]!=0){
+              int temp=arr[i];
+              arr[i]=arr[j];
+              arr[j]=temp;
+                i++;
+                j++;
             }
-            
+            else{
+                j++;
+            }
         }
     }
 }
