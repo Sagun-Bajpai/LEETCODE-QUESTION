dcd8c0d7e842a82d83051454bc7d9dd88df56b64
Sagun Bajpai <sagunbajpaisagun@gmail.com>
2026-08-22 20:31:28 +0530

    Time: 2287 ms (5.11%) | Memory: 50.6 MB (99.92%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int majorityElement(int[] arr) {
+        int n = arr.length;
+        int max = 0;
+        if (n == 1) {
+            return arr[0];
+        }
+        int ans = arr[0];
+
+        for (int i = 0; i < n - 1; i++) {
+            int count = 1;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                }
+
+            }
+            if (max < count) {
+
+                max = count;
+                ans = arr[i];
+            }
+            System.out.println(max);
+        }
+        if (max > n / 2) {
+            return ans;
+        }
+        return 0;
+
+    }
+}
