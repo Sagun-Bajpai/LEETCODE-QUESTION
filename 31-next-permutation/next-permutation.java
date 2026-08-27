2d1f7aae36bbf1599245918e69e5f94d448da112
Sagun Bajpai <sagunbajpaisagun@gmail.com>
2026-08-27 20:52:04 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (15.97%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -14,25 +14,29 @@ class Solution {
     public void nextPermutation(int[] arr) {
         int pivot=-1;
         int n=arr.length;
+         // Step 1: find pivot (start from n-2 to avoid out-of-bounds)
         for(int i=n-2;i>=0;i--){
             if(arr[i]<arr[i+1]){
                 pivot=i;
                 break;
             }
         }
-
+        
+          // Step 2: if no pivot, whole array is descending -> reverse it fully
         if(pivot==-1){
             reverse(arr, 0, n - 1);
             return;
         }
-
+        
+        // Step 3: find element just bigger than pivot, from the right
         for(int i=n-1;i>pivot;i--){
             if(arr[i]>arr[pivot]){
                 swap(arr,i,pivot);
                 break;
             }
         }
-
+        
+        // Step 4: reverse everything after pivot
         int i=pivot+1;
         int j=n-1;
         while(i<=j){
